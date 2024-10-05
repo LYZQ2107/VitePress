31884f1bd807269648480dbe98c4829de13152b1
LYZQ2107 <109856870+LYZQ2107@users.noreply.github.com>
2024-10-05 16:00:20 +0800

    Update nav.ts

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -1,15 +1,13 @@
 import type { DefaultTheme } from 'vitepress'
 
 export const nav: DefaultTheme.Config['nav'] = [
-  { text: '前端导航', link: '/nav/' },
-  { text: '茂茂主页', link: 'https://fe-mm.com' },
+  { text: '导航', link: '/nav' },
+  { text: '自定义导航', link: '/nav' },
   {
-    text: '茂茂物语',
-    link: 'https://notes.fe-mm.com',
-  },
-  { text: 'mmPlayer', link: 'https://netease-music.fe-mm.com' },
-  {
-    text: '油猴脚本',
-    link: 'https://github.com/maomao1996/tampermonkey-scripts',
-  },
-]
+    text: '友情链接',
+    items: [
+      { text: '星渊基石', link: 'https://idc.oteam.top' },
+      { text: '服务器状态监测', link: 'https://bj-xq27.oteam.top' }
+    ]
+  }
+],
